47abe249fea6bab507174dc1cc23b1d8256af34a
Neil Pate <neil@premiergsolutions.com>
2023-11-25 20:27:41 +0100

    feat: Initial attempt at I2C comms

--- src/main.rs
+++ src/main.rs
@@ -3,164 +3,111 @@
 
 use cortex_m_rt::entry;
 use panic_halt as _;
-use stm32f3::stm32f303::interrupt;
 
 // Reset & Clock Control
 const RCC_ADDR: u32 = 0x4002_1000;
-const RCC_AHBENR_OFFSET: u32 = 0x14; // Advanced High Performance Bus Enable Register
-const RCC_AHBENR: u32 = RCC_ADDR + RCC_AHBENR_OFFSET;
+const RCC_AHB1ENR_OFFSET: u32 = 0x14; // Advanced High Performance Bus Enable Register
 const RCC_AHB2ENR_OFFSET: u32 = 0x18;
-const RCC_AHB2ENR: u32 = RCC_ADDR + RCC_AHB2ENR_OFFSET;
+const RCC_APB1ENR_OFFSET: u32 = 0x40; // Peripheral clock enable Register
 
-// General Purpose IO Port A
-const GPIOA_ADDR: u32 = 0x4800_0000;
-const GPIOA_IDR_OFFSET: u32 = 0x10; // Input Data Register
-const GPIOA_IDR_ADDR: u32 = GPIOA_ADDR + GPIOA_IDR_OFFSET;
+// GPIO
+const MODER_OFFSET: u32 = 0x0; // Mode Register
+const OTYPER_OFFSET: u32 = 0x4; // Output Type Register
+const PUPDR_OFFSET: u32 = 0x0C; // Pull-up/Pull-down Register
+const IDR_OFFSET: u32 = 0x10; // Input Data Register
+const AFRL_OFFSET: u32 = 0x20; // Alternate Function Low Register
+const AFRH_OFFSET: u32 = 0x24; // Alternate Function High Register
 
-// General Purpose IO Port E
-const GPIOE_ADDR: u32 = 0x4800_1000;
-const GPIO_MODER_OFFSET: u32 = 0x00; // Mode Register
-const GPIO_BSRR_OFFSET: u32 = 0x18; // Bit Set/Reset Register
-const GPIOE_MODER_ADDR: u32 = GPIOE_ADDR + GPIO_MODER_OFFSET;
-const GPIOE_BSRR_ADDR: u32 = GPIOE_ADDR + GPIO_BSRR_OFFSET;
+// General Purpose IO Port B
+const GPIOB_ADDR: u32 = 0x4800_0400;
 
-// Interrupts
-const SYSCFG_ADDR: u32 = 0x4001_0000;
-const SYSCFG_EXTICR1_OFFSET: u32 = 0x8;
-const SYSCFG_EXTICR1_ADDR: u32 = SYSCFG_ADDR + SYSCFG_EXTICR1_OFFSET;
+// I2C Peripheral
+const I2C1_ADDR: u32 = 0x4000_5400;
+const I2C_CR1_OFFSET: u32 = 0x00;
+const I2C_CR2_OFFSET: u32 = 0x04;
+const I2C_CCR_OFFSET: u32 = 0x1c;
+const I2C_TRISE_OFFSET: u32 = 0x20;
 
-const EXTI_ADDR: u32 = 0x4001_0400;
-const EXTI_IMR1_OFFSET: u32 = 0x00; // Input mask
-const EXTI_RTSR1_OFFSET: u32 = 0x08; // Rising trigger select register
-const EXTI_FTSR1_OFFSET: u32 = 0x0C; // Falling trigger select register
-const EXTI_PR1_OFFSET: u32 = 0x14; // Pending
-
-// Nested Vector Inierrupt Controller
-const NVIC_ADDR: u32 = 0xe000_e100;
-const NVIC_ISER0_OFFSET: u32 = 0x0; // Interrupt Set Enable
-
-const OUTPUT_PIN: i32 = 13; // On the STM32F3Discovery the West LED of the compass (green) is Port E.13
-
-fn setup_gpioe_pin_as_output(pin: i32) -> () {
+fn setup_clocks() -> () {
     unsafe {
-        // Enable the GPIOE peripheral
-        let rcc_ahbenr = &*(RCC_AHBENR as *mut volatile_register::RW<u32>);
-        rcc_ahbenr.modify(|r| r | (1 << 21)); // Bit 21 is the I/O port E clock enable
+        // Enable the GPIOB peripheral
+        let ahbenr = &*((RCC_ADDR + RCC_AHB1ENR_OFFSET) as *mut volatile_register::RW<u32>);
+        ahbenr.modify(|r| r | (1 << 18)); // Bit 18 is the I/O port B clock enable
 
-        // Set desired pin as output, controlled by MODER
-        let gpioe_moder = &*(GPIOE_MODER_ADDR as *mut volatile_register::RW<u32>);
-        let pin_shift = pin * 2; // Calculate the bit position based on pin number
+        // Enable the I2C peripheral
+        let apb1enr = &*((RCC_ADDR + RCC_APB1ENR_OFFSET) as *mut volatile_register::RW<u32>);
+        apb1enr.modify(|r| r | (1 << 21)); // Bit 21 enables the clock to the I2C periphweral
+    }
+}
+
+fn setup_i2c_peripheral() -> () {
+    unsafe {
+        // Reset the peripheral
+        let cr1 = &*((I2C1_ADDR + I2C_CR1_OFFSET) as *mut volatile_register::RW<u32>);
+        cr1.modify(|r| r | (1 << 15)); // Bit 15 is the software reset bit
+        cr1.modify(|r| r | (0 << 15)); // clear the reset
+
+        // Set clock to the the peripheral
+        let cr2 = &*((I2C1_ADDR + I2C_CR2_OFFSET) as *mut volatile_register::RW<u32>);
+        cr2.write(45); // 45 MHz
+
+        // Set clock to the the peripheral
+        let ccr = &*((I2C1_ADDR + I2C_CCR_OFFSET) as *mut volatile_register::RW<u32>);
+        ccr.write(225); //
+
+        // Set rise time
+        let trise = &*((I2C1_ADDR + I2C_TRISE_OFFSET) as *mut volatile_register::RW<u32>);
+        trise.write(46); //
+
+        cr1.modify(|r| r | (1 << 0)); // Enable the peripheral
+    }
+}
+
+fn setup_i2c_pin(pin: i32) -> () {
+    // Note this will only work for pins 0..7
+
+    unsafe {
+        // Set pin as alternate function, controlled by MODER
+        let moder = &*((GPIOB_ADDR + MODER_OFFSET) as *mut volatile_register::RW<u32>);
+        let pin_shift = pin * 2; // Calculate the bit position based on pin number, 2 bits per pin
         let mask = 0b11 << pin_shift; // Create a mask for the pin bits in the register (2 bits per pin)
-        let mode = 0b01; // General purpose output mode
+        let mode = 0b10; // Alternate function mode
         let set_mode = mode << pin_shift; // Shift the mode to the correct position
+        moder.modify(|r| (r & !mask) | set_mode); // First clear the two bits of this pins mode, then OR with the new (bit-shifted) value
 
-        gpioe_moder.modify(|r| (r & !mask) | set_mode); // First clear the two bits of this pins mode, then OR with the new (bit-shifted) value
+        // Set output to Open Drain
+        let otyper = &*((GPIOB_ADDR + OTYPER_OFFSET) as *mut volatile_register::RW<u32>);
+        otyper.modify(|r| r | (1 << pin)); // Set bit <pin>
+
+        // No need to enable pull-up resistors as R27 and R28 are present
+        //let pupdr = &*((GPIOB_ADDR + PUPDR_OFFSET) as *mut volatile_register::RW<u32>);
+
+        // Set alternate function 4
+        let afrl = &*((GPIOB_ADDR + AFRL_OFFSET) as *mut volatile_register::RW<u32>);
+        let pin_shift = pin * 4; // Calculate the bit position based on pin number, there are 4 bits per pin
+        let mask = 0b1111 << pin_shift; // Create a mask for the pin bits in the register (4 bits per pin)
+        let set_afr4 = 0b0100; // Alternate Function 4 (I2C)
+        afrl.modify(|r| (r & !mask) | set_afr4); // First clear the four bits of this pins alternate function, then OR with the new (bit-shifted) value
     }
 }
 
-fn setup_gpioa_pin_as_input() -> () {
-    // On the STM32F3Discovery the USER pushbutton is connected to Port A.0
-
+fn i2c_start() -> () {
     unsafe {
-        // Enable the GPIOA peripheral
-        let ahbenr = &*(RCC_AHBENR as *mut volatile_register::RW<u32>);
-        ahbenr.modify(|r| r | (1 << 17)); // Bit 17 is the I/O port A clock enable
-
-        // By default the pin is set to be an input, so no further config is needed
-    }
-}
-
-fn setup_input_interrupt() -> () {
-    unsafe {
-        // Enable the clock to the SYSCFG peripheral
-        let ahb2enr = &*(RCC_AHB2ENR as *mut volatile_register::RW<u32>);
-        ahb2enr.modify(|r| r | (1 << 0)); //Set Bit 0 (SYSCFGEN) to enable System Config clock
-
-        // Connect EXTI0 line to Port A.0
-        let exticr1 = &*(SYSCFG_EXTICR1_ADDR as *mut volatile_register::RW<u32>);
-        exticr1.modify(|r| r & 0b000); //Set lowest 3 bits 000 of SYSCFG_EXTICR1 to enable select Port A.0
-
-        // The pin needs to be set to an input, but this is the default after reset so no action needed
-        // This is controlled by GPIOA_MODER
-
-        // Configure the interrupt source
-        // This is controlled by SYSCFG_EXTICR1
-        // On reset Port A.0 is already configured so no action is necessary
-
-        // Unmask interrupt for Port A.0
-        // This is controlled in EXTI_IMR1
-        let exti_imr1 = &*((EXTI_ADDR + EXTI_IMR1_OFFSET) as *mut volatile_register::RW<u32>);
-        exti_imr1.modify(|r| r | (1 << 0));
-
-        // Configure the trigger to use a rising trigger
-        // This is controlled by EXTI_RTSR1
-        let exti_rtsr1 = &*((EXTI_ADDR + EXTI_RTSR1_OFFSET) as *mut volatile_register::RW<u32>);
-        exti_rtsr1.modify(|r| r | (1 << 0));
-
-        // Configure the trigger to use a rising trigger
-        // This is controlled by EXTI_FTSR1
-        let exti_ftsr1 = &*((EXTI_ADDR + EXTI_FTSR1_OFFSET) as *mut volatile_register::RW<u32>);
-        exti_ftsr1.modify(|r| r | (1 << 0));
-
-        // Enable the interrupt
-        // This is controlled by NVIC_ISER0 (Interrupt Set Enable)
-        let nvic_iser0 = &*((NVIC_ADDR + NVIC_ISER0_OFFSET) as *mut volatile_register::RW<u32>);
-        nvic_iser0.modify(|r| r | (1 << 6));
-    }
-}
-
-fn set_led_on(pin: i32) -> () {
-    unsafe {
-        // BSRR is the register used to set or clear individual pins
-        let bsrr = &*(GPIOE_BSRR_ADDR as *mut volatile_register::RW<u32>);
-        bsrr.write(1 << pin); // A pin is set by setting the bit in the lower 16 bits of the BSRR
-    }
-}
-
-fn set_led_off(pin: i32) -> () {
-    unsafe {
-        // BSRR is the register used to set or clear individual pins
-        let bsrr = &*(GPIOE_BSRR_ADDR as *mut volatile_register::RW<u32>);
-        bsrr.write(1 << 16 + pin); // A pin is cleared by setting the bit in the top 16 bits of the BSRR
-    }
-}
-
-fn read_input() -> bool {
-    unsafe {
-        // IDR is the value at the input of the port
-        let idr = &*(GPIOA_IDR_ADDR as *mut volatile_register::RW<u32>);
-        let value = idr.read();
-
-        let mask = 0x1;
-
-        value & mask > 0
-    }
-}
-
-#[interrupt]
-fn EXTI0() {
-    unsafe {
-        // PR1 is the Pending Registers, which indicates which triggers have occurred
-        let exti_pr1 = &*((EXTI_ADDR + EXTI_PR1_OFFSET) as *mut volatile_register::RW<u32>);
-        exti_pr1.modify(|r| (r | 0x1)); // Set the bit to clear the pending interrupt
-    }
-
-    let switch_pressed = read_input();
-
-    if switch_pressed {
-        set_led_on(OUTPUT_PIN);
-    } else {
-        set_led_off(OUTPUT_PIN);
+        // ACK
+        let cr1 = &*((I2C1_ADDR + I2C_CR1_OFFSET) as *mut volatile_register::RW<u32>);
+        cr1.modify(|r| r | (1 << 10)); // Bit 10 is the Ack bit
+        cr1.modify(|r| r | (1 << 8)); // Bit 8 is the start bit
     }
 }
 
 #[entry]
 fn main() -> ! {
-    setup_gpioe_pin_as_output(OUTPUT_PIN); // There are 8 LED outputs connected to Port E.8 --> 15. Change OUTPUT_PIN to use a different LED
+    setup_clocks();
+    setup_i2c_peripheral();
+    setup_i2c_pin(6); // SCL is Port B.6
+    setup_i2c_pin(7); // SDA is Port B.7
 
-    setup_gpioa_pin_as_input(); // On the STM32F3Discovery the USER pushbutton is connected to Port A.0
-
-    setup_input_interrupt();
+    i2c_start();
 
     loop {}
 }
